f236c47b58f28df4169dc93fb0da65cd23649bf9
grtaniashvili <147092168+grtaniashvili@users.noreply.github.com>
2023-10-26 20:35:36 +0400

    Add files via upload

---
+++ SQL_HW1.sql
@@ -0,0 +1,28 @@
+﻿ --Create Database
+ create database SkillWill
+ -- Create Table
+ use SkillWill
+ create table dbo.SkillWillHW1( ID int primary key
+                                ,First_Name nvarchar(20)
+							    ,LastName nvarchar(50)
+							    ,age int)
+
+--Insert Into
+insert into dbo.SkillWillHW1
+values (1,N'გრიგოლ', N'თანიაშვილი', 30)
+		,(2,N'გიორგი', N'თანიაშვილი', 23)
+		,(3,N'მარიამ', N'თანიაშვილი', 55)
+		,(4,N'ანა', N'თანიაშვილი', 21)
+
+-- Select
+select *
+from dbo.SkillWillHW1
+where ID in (2,4)
+-- Update
+update a
+set a.age = 100
+from dbo.SkillWillHW1 a 
+where ID = 1
+--Delete
+delete from dbo.SkillWillHW1
+where ID not in (2,4)
