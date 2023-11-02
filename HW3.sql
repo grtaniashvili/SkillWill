2d863c080605750017be0fac89aa589025723e99
grtaniashvili <147092168+grtaniashvili@users.noreply.github.com>
2023-11-02 23:45:35 +0400

    Add files via upload

---
+++ HW3.sql
@@ -0,0 +1,34 @@
+drop table if exists #test
+--
+create table #test ( 
+					id int identity(1,1) primary key
+					,num bigint
+					)
+--
+declare @i int = 0, @stime datetime, @etime datetime
+--
+while @i < 1000000
+begin
+	insert into #test (num) values (@i)
+	set @i += 1
+end
+--
+set @stime = GETDATE()
+--
+select num
+from #test
+where cast(num an varchar(10)) like '
+set @etime = GETDATE()
+--
+select datediff(SS,@stime,@etime)
+--
+create index i_num on #test (num)
+--
+set @stime = GETDATE()
+--
+select num
+from #test
+where num %5 = 1
+set @etime = GETDATE()
+--
+select datediff(ss,@stime,@etime)
